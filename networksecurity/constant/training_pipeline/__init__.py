32bc00beacbccf72e937261c85b3905abc56ea0d
Aditya-j101 <157642554+Aditya-j101@users.noreply.github.com>
2026-03-08 01:25:04 +0530

    Model Training in Process

--- networksecurity/constant/training_pipeline/__init__.py
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -14,6 +14,11 @@ TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv" 
 
 SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+
+SAVED_MODEL_DIR = os.path.join("saved_models")
+MODEL_FILE_NAME = "model.pkl"
+
+
 """Data ingestion related constant start with data ingestion VAR NAME"""
 
 DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
@@ -46,6 +51,7 @@ DATA_TRANSFORMATION_IMPUTER_PARAMS: dict = {
 }
 DATA_TRANSFORMATION_TRAIN_FILE_PATH: str = "train.npy"
 DATA_TRANSFORMATION_TEST_FILE_PATH: str = "test.npy"
+
 """MODEL TRAINER RELATED CONSTANT START WITH MODE TRAINER VAR NAME """
 MODEL_TRAINER_DIR_NAME: str = "model_trainer"
 MODEL_TRAINER_TRAINED_MODEL_DIR: str = "trained_model"
